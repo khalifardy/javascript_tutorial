de8ae251dde38b05986c3a136aa3278de59599c2
khalifardy <khalifardy.miqdarsah@gmail.com>
2024-07-15 04:10:47 +0700

    second

---
+++ latihan.js
@@ -0,0 +1,8 @@
+let kata;
+let count = 0;
+
+while (count < 100) {
+  count++;
+  kata = "angkot no." + count + " beroperasi dengan baik";
+  console.log(kata);
+}
---
+++ wps_tutorial1.js
@@ -0,0 +1,7 @@
+var nama = prompt("Masukkan nama Anda: ");
+var tes = confirm("Apakah Anda yakin nama Anda adalah " + nama + "?");
+if (tes == true) {
+  alert("Asslamualaikum " + nama + ", selamat datang di WPS Tutorial!");
+} else {
+  alert("Silahkan masukkan nama Anda kembali!");
+}
